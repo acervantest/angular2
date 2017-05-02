f4a3724ffa1c30a35d5326b375110cd8037e7d41
alejandro <alejandro@alejandros-Air.hitronhub.home>
2017-05-01 22:58:55 -0700

    hobbies and button

--- src/app/components/user.component.js
+++ src/app/components/user.component.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var UserComponent = (function () {
@@ -14,16 +17,27 @@ var UserComponent = (function () {
         this.address = {
             street: '777 East Kings Rd',
             city: 'North Vancouver',
-            state: 'British Columbia',
+            state: 'British Columbia'
         };
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = true;
     }
+    UserComponent.prototype.toggleHobbies = function () {
+        if (this.showHobbies == true) {
+            this.showHobbies = false;
+        }
+        else {
+            this.showHobbies = true;
+        }
+    };
     return UserComponent;
 }());
 UserComponent = __decorate([
     core_1.Component({
         selector: 'user',
-        template: "\n    <h1>Hello {{name}}</h1>\n    <p><strong>Email:</strong>  {{ email }}</p>\n    <p><strong>Address:</strong>  {{ address.street }} {{ address.city }} {{ address.state }}</p>\n\n",
-    })
+        template: "\n    <h1>Hello {{name}}</h1>\n    <p><strong>Email:</strong>  {{ email }}</p>\n    <p><strong>Address:</strong>  {{ address.street }} {{ address.city }} {{ address.state }}</p>\n    \n    \n    <button (click)=\"toggleHobbies()\">{{ showHobbies ? \"Hide Hobbies\" : \"Show Hobbies\" }}</button>\n    <div *ngIf=\"showHobbies\">\n        <h3>Hobbies</h3>\n        <ul>\n            <li *ngFor=\"let hobby of hobbies\">\n                {{ hobby }}\n            </li>\n        </ul>\n    </div>\n    ",
+    }),
+    __metadata("design:paramtypes", [])
 ], UserComponent);
 exports.UserComponent = UserComponent;
 //# sourceMappingURL=user.component.js.map
--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -6,16 +6,50 @@ import { Component } from '@angular/core';
     <h1>Hello {{name}}</h1>
     <p><strong>Email:</strong>  {{ email }}</p>
     <p><strong>Address:</strong>  {{ address.street }} {{ address.city }} {{ address.state }}</p>
-
-`,
+    
+    
+    <button (click)="toggleHobbies()">{{ showHobbies ? "Hide Hobbies" : "Show Hobbies" }}</button>
+    <div *ngIf="showHobbies">
+        <h3>Hobbies</h3>
+        <ul>
+            <li *ngFor="let hobby of hobbies">
+                {{ hobby }}
+            </li>
+        </ul>
+    </div>
+    `,
 })
 
 export class UserComponent { 
-    name  = 'Alex'; 
-    email = 'acervantes@gmail.com';
-    address = {
-        street: '777 East Kings Rd',
-        city: 'North Vancouver',
-        state: 'British Columbia',
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+    
+    constructor(){
+        this.name  = 'Alex'; 
+        this.email = 'acervantes@gmail.com';
+        this.address = {
+            street: '777 East Kings Rd',
+            city: 'North Vancouver',
+            state: 'British Columbia'
+        }
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = true;
     }
+    
+    toggleHobbies(){
+        if(this.showHobbies == true){
+            this.showHobbies = false;
+        }else{
+            this.showHobbies = true;
+        }
+    }
+}
+
+interface address {
+    street: string;
+    city: string;
+    state: string;
 }
